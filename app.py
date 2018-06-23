d31530ff1dd4db2817c91d021248d73c9306ffb4
vineethfull <vineeth.purushothaman@anywhere.co>
2018-06-24 00:27:16 +0530

    evn changes

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 
-@app.route("/")
+@app.route('/')
 def hello():
     return "Hello World!"
 
-if __name__ =='__main__':
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
     app.run()
+
 
 
